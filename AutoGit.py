abfd865df21693180d3d82b0b3fcc1755f5e67b5
TheGlitchHam <faross@online.de>
2020-12-30 18:25:55 +0100

    Minor bug fix, now returns repo url

--- AutoGit.py
+++ AutoGit.py
@@ -26,6 +26,7 @@ def getGithubInstance():
 
 
 def getGitUsername():
+    """ unused method for getting the git username"""
     try:
         with open("username", "r") as f:
             return f.read()
@@ -38,47 +39,53 @@ def getGitUsername():
 
 
 def createConf():
+    """creates and returns config for github"""
     conf = {"private": False, "init": False, "gitignore": ""}
 
-    if input("Use existing config? Type y/n: ").lower() == "y":
-        try:
-            with open("conf", "r") as f:
-                conf = json.load(f)
+    try:
+        with open("conf", "r") as f:
+            conf = json.load(f)
+            print("Config found: ")
+            print(conf)
+            if input("Use the existing config? Type y/n: ").lower() == "y":
                 return conf
-        except Exception as e:
-            print(
-                "Config could not be loaded. Problem with file, or file doesn't exist! " + str(e))
-    else:
-        if input("Do you want to build a private repo? Type y/n: ").lower() == "y":
-            conf["private"] = True
 
-        if input("Do you want to init the repo? y/n: ").lower() == "y":
-            conf["init"] = True
+    except Exception as e:
+        print(
+            "Config could not be loaded. Problem with file, or file doesn't exist! " + str(e))
 
-        if input("Do you want to use a gitignore template? y/n: ").lower() == "y":
-            getGithubInstance().get_gitignore_templates()
-            conf["gitignore"] = input(
-                "Enter Template name (e.g. Python, Ruby): ")
+    if input("Do you want to build a private repo? Type y/n: ").lower() == "y":
+        conf["private"] = True
 
-        if input("Do you want to safe the current config? Type y/n: ").lower() == "y":
-            json.dump(conf, open("conf", "w"))
-            print("config saved")
+    if input("Do you want to init the repo? y/n: ").lower() == "y":
+        conf["init"] = True
+
+    if input("Do you want to use a gitignore template? y/n: ").lower() == "y":
+        getGithubInstance().get_gitignore_templates()
+        conf["gitignore"] = input(
+            "Enter Template name (e.g. Python, Ruby): ")
+
+    if input("Do you want to safe the current config? Type y/n: ").lower() == "y":
+        json.dump(conf, open("conf", "w"))
+        print("config saved")
 
     return conf
 
 
 def githubInit():
+    """ create new repository """
 
     repo_name = input("Please enter a repo name: ")
 
     conf = createConf()
 
-   # try:
-    getGithubInstance().get_user().create_repo(name=repo_name, private=conf["private"], auto_init=conf["init"],
-                                               gitignore_template=conf["gitignore"], description=input("Enter a description: "))
+    try:
+        g = getGithubInstance().get_user().create_repo(name=repo_name, private=conf["private"], auto_init=conf["init"],
+                                                       gitignore_template=conf["gitignore"], description=input("Enter a description: "))
 
-    # except Exception as e:
-    #     print("Something went wrong, exception: " + str(e))
+        print("Success! \n%s" % g.clone_url)
+    except Exception as e:
+        print("Something went wrong, exception: " + str(e))
 
 
 if __name__ == "__main__":
